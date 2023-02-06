687f9dd63444c93aea17e991dfbb91bb4af9e2b7
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-02-06 17:13:49 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Task_2
+        String path = "folder1/folder2/folder3";
+        File directories = new File(path);
+        List<File> files = new ArrayList<>();
+        files.add(new File(path + "/A1.txt"));
+        files.add(new File(path + "/A2.txt"));
+        files.add(new File(path + "/A3.txt"));
+        files.add(new File(path + "/B1.txt"));
+        files.add(new File(path + "/B2.txt"));
+        directories.mkdirs();
+        for (File file : files) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        File[] filtered = directories.listFiles((dir, name) -> name.startsWith("A"));
+        for (File file : filtered) {
+            System.out.println(file.getAbsolutePath());
+        }
+        //Task_1
+        /*
         File directory = new File("folder/folder2/folder1");
         File file1 = new File("folder/folder2/folder1/file1.txt");
         File file2 = new File("folder/folder2/folder1/file2.txt");
@@ -15,12 +42,13 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        File[] files = directory.listFiles();
+        File[] files = directory.listFiles((dir, name) -> name.contains("1"));
         for (File file : files) {
             System.out.println(file.getName());
         }
-        //System.out.println(file1.exists());
-        //System.out.println(directory.isDirectory());
-        //System.out.println(file1.isFile());
+        System.out.println(file1.exists());
+        System.out.println(directory.isDirectory());
+        System.out.println(file1.isFile());
+        */
     }
 }
