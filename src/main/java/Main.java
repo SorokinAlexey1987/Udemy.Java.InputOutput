e972d4ea8c0a8acbee4ed892467a1f61c170e903
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-02-06 17:42:30 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,51 @@
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Task_4
+        File directory = new File("folder");
+        directory.mkdir();
+        File file = new File(directory, "names.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (InputStream inputStream = new FileInputStream(file)) {
+            int a = inputStream.read();
+            while (a != -1) {
+                System.out.print((char) a);
+                a = inputStream.read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Task_3
+        /*
+        File file = new File("1.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (InputStream inputStream = new FileInputStream(file)) {
+            int a = inputStream.read();
+            while (a != -1) {
+                System.out.print((char) a);
+                a = inputStream.read();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        */
+
+
         //Task_2
+        /*
         String path = "folder1/folder2/folder3";
         File directories = new File(path);
         List<File> files = new ArrayList<>();
@@ -28,6 +66,8 @@ public class Main {
         for (File file : filtered) {
             System.out.println(file.getAbsolutePath());
         }
+        */
+
         //Task_1
         /*
         File directory = new File("folder/folder2/folder1");
