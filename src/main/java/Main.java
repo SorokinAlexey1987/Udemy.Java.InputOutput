e019c0f36fccb454bcff198d911f58b115bfe32e
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-02-12 16:57:42 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        //Task_6
+        File directory = new File("folder");
+        File file = new File(directory, "names.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
+            char[] array = new char[128];
+            int count = reader.read(array);
+            StringBuilder result = new StringBuilder();
+            while (count > 0) {
+                result.append (new String(array, 0, count));
+                count = reader.read(array);
+            }
+            String[] names = result.toString().split(" ");
+            Arrays.stream(names)
+                    .filter(name -> name.startsWith("A"))
+                    .forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         //Task_5
+        /*
         File directory = new File("folder");
         directory.mkdir();
         File file = new File(directory, "names.txt");
@@ -11,18 +38,18 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        try (InputStream inputStream = new FileInputStream(file)) {
-            byte[] array = new byte[8];
-            int count = inputStream.read(array);
+        try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
+            int a = reader.read();
             StringBuilder result = new StringBuilder();
-            while (count > 0) {
-                result.append (new String(array, 0, count));
-                count = inputStream.read(array);
+            while (a > 0) {
+                result.append ((char) a);
+                a = reader.read();
             }
             System.out.println(result.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
+        */
 
 
         //Task_4
